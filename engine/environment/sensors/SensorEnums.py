feef00c7fa9605b7b35cb8f8368ed5fbe7e1f455
ted <tmcculloch@g.harvard.edu>
2025-04-08 14:04:02 -0400

    added documentation

--- engine/environment/sensors/SensorEnums.py
+++ engine/environment/sensors/SensorEnums.py
@@ -1,9 +1,23 @@
 from enum import Enum
 
 class GroundSensorModality(Enum):
-    RADAR  = 1 # RADAR
-    OPTICS = 2 # Optical Telescope
-    
+    """
+    Enum representing different modalities of ground sensors.
+
+    Attributes:
+        RADAR (int): Represents a RADAR sensor.
+        OPTICS (int): Represents an Optical Telescope sensor.
+    """
+    RADAR = 1  # RADAR
+    OPTICS = 2  # Optical Telescope
+
 class SensorGeneralStatus(Enum):
-    AVAILABLE  = "AVAILABLE" # (night for optics)
-    NOT_AVAILABLE = "NOT_AVAILABLE" # (day for optics)
+    """
+    Enum representing the general status of a sensor.
+
+    Attributes:
+        AVAILABLE (str): Indicates that the sensor is available (e.g., night for optics).
+        NOT_AVAILABLE (str): Indicates that the sensor is not available (e.g., day for optics).
+    """
+    AVAILABLE = "AVAILABLE"  # (night for optics)
+    NOT_AVAILABLE = "NOT_AVAILABLE"  # (day for optics)
--- engine/environment/sensors/SensorInfo.py
+++ engine/environment/sensors/SensorInfo.py
@@ -1,5 +1,26 @@
 class SensorInfo:
-    def  __init__(self, name, lla, modality):
+    """
+    A class to represent a sensor's basic information.
+
+    Attributes:
+        name (str): The name of the sensor.
+        lla (list): length 3 list representing the sensor's location in latitude, longitude, and altitude (LLA).
+        modality (engine.environment.sensors.SensorEnums.GroundSensorModality): The modality or type of sensor 
+
+    Methods:
+        __init__(self, name, lla, modality):
+            Initializes a new instance of the SensorInfo class.
+    """
+
+    def __init__(self, name, lla, modality):
+        """
+        Initializes a new instance of the SensorInfo class.
+
+        Args:
+            name (str): The name of the sensor.
+            lla (tuple): A tuple representing the sensor's location in latitude, longitude, and altitude.
+            modality (engine.environment.sensors.SensorEnums.GroundSensorModality): The modality or type of sensor 
+        """
         self.name = name
         self.lla = lla
         self.modality = modality

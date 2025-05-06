5c09e90c62045be321f3dadb8d353d73ca81c991
ted <tmcculloch@g.harvard.edu>
2025-05-06 19:34:12 -0400

    progress on s3 and s4

--- scripts/scenario4/algs-eval.py
+++ scripts/scenario4/algs-eval.py
@@ -8,13 +8,13 @@ from engine.util.plots import basic_ground_sensor_plot_v1, basic_uncertainty_plo
 import pickle
 
 
-EXPERIMENT_NAME = "S3"
+EXPERIMENT_NAME = "S4"
 
-BASE_PATH = './scripts/scenario3/'
+BASE_PATH = './scripts/scenario4/'
 N_ROUNDS = 100
 
-sat_keys = ["AEHF 1", "AEHF 2", "AEHF 3", "AEHF 4", "MUOS", "MUOS 3"]
-sensor_keys = ['mhr', 'ascension', 'socorro']
+sat_keys = ["AEHF 1", "AEHF 2", "AEHF 3", "AEHF 4", "MUOS", "MUOS 3", "MUOS 5", "GOES 1", "GOES 5", "GOES 6", "GOES 7", "AMC 3"]
+sensor_keys = ['mhr', 'ascension', 'socorro', 'vandenberg']
 
 #AGENT = "DN"
 #Agents = [DoNothingAgent(AGENT,sensor_keys, sat_keys)]
@@ -22,21 +22,21 @@ sensor_keys = ['mhr', 'ascension', 'socorro']
 #AGENT = "smart-random"
 #Agents = [RandomAgent(AGENT,sensor_keys, sat_keys)]
 
-#AGENT = "revisit"
-#Agents = [BasicRevisitAgent(AGENT,sensor_keys, sat_keys)]
+AGENT = "revisit"
+Agents = [BasicRevisitAgent(AGENT,sensor_keys, sat_keys)]
 
 
-AGENT = "q-table"
-with open(BASE_PATH+"agent-q-table-S3.pkl", "rb") as f:
-    q_table_agent = pickle.load(f)
+#AGENT = "q-table"
+#with open(BASE_PATH+"agent-q-table-S4.pkl", "rb") as f:
+#    q_table_agent = pickle.load(f)
     
-Agents = [q_table_agent]  
+#Agents = [q_table_agent]  
 
 
 
 sim_track = SimOutcomeTracker(EXPERIMENT_NAME+'-'+AGENT+"-eval",sensor_keys, sat_keys, N_ROUNDS)
 
-env = Environment(sensor_keys, sat_keys, randomizer=Randomizer(scenario_length_hrs=[24,24], maneuver_details=["type1", [2,2], [1,2], [0.5,11]])) 
+env = Environment(sensor_keys, sat_keys, randomizer=Randomizer(scenario_length_hrs=[24,24], maneuver_details=["type1", [3,6], [1,2], [0.5,11]])) 
 for i in range(N_ROUNDS):
     t, state_cat,events_out, Done = env.reset()
     
